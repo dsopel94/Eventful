ee5ec9f3851419b46b435b95ac842d2e7617a43a
Damian Sopel <dsopel0@gmail.com>
2017-01-08 23:22:26 -0600

    Final revision

--- js.js
+++ js.js
@@ -1,39 +1,36 @@
 $(document).ready(function() {
-    $('.findevents').submit( function(event){
+  $('.findevents').submit(function(event) {
     event.preventDefault();
     var event = $(this).find("input[name='event']").val();
     var location = $(this).find("input[name='zip']").val();
-    showEvents(event,location);
-    console.log(location);
-    $("#ListItems").html('');
-    });
+    showEvents(event, location);
+  });
 });
 
-function showEvents (e,l) {
+function showEvents(e, l) {
 
-var oArgs = {
-app_key: "rqJNStmNMPNtvngf",
-q: e,
-where: l,
-page_size: 10,
-sort_order: "popularity",
- date:"This week",
-};
- var content = '';
-EVDB.API.call("/events/search", oArgs, function(oData) {
-            console.log(oData)
-            //Get the title for each item
-            for (var i = 0; i < oData.events.event.length;i++) {
-                content += 
-                 '<div class="box"><a href=' +
-                oData.events.event[i].url + '>' +
-                 oData.events.event[i].title + '</a>' + 
-                " at the " + oData.events.event[i].venue_name +
-                " Start Date and Time: " + oData.events.event[i].start_time
-
-                + '</div>';
-            }
-            // Show Data on page
-         $("#ListEvents").html(content);
-        });
-    }
+  var oArgs = {
+    app_key: "rqJNStmNMPNtvngf",
+    q: e,
+    where: l,
+    page_size: 10,
+    sort_order: "popularity",
+    date: "This week",
+  };
+ 
+  EVDB.API.call("/events/search", oArgs, function(oData) {
+    console.log(oData);
+      //Get the title for each item
+      var content = oData.events.event.map(function(item) { 
+        return '<div class="box"><a href=' +
+        item.url + '>' +
+        item.title + '</a>' +
+        " at the " + item.venue_name +
+        " Start Date and Time: " +
+        item.start_time +
+        '</div>'; 
+      }); 
+    // Show Data on page
+    $("#ListEvents").html(content);
+  });
+}
